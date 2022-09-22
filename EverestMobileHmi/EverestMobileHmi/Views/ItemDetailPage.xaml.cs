1359d7926bca5415c3a1871a7efd3a84946feedb
Wesley Vercoutere <32158471+WesleyVercoutere@users.noreply.github.com>
2022-09-22 13:20:37 +0200

    Project Setup

---
+++ EverestMobileHmi/EverestMobileHmi/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using EverestMobileHmi.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace EverestMobileHmi.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
---
+++ EverestMobileHmi/EverestMobileHmi/Views/NewItemPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using EverestMobileHmi.Models;
+using EverestMobileHmi.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace EverestMobileHmi.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
